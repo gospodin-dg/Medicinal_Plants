7c3cd0879bb11f1264a2d4c6b4bffccfdbcab78c
Gospodin-DG <gospodin-dg@yandex.ru>
2022-01-20 20:44:44 +0300

    Add "About the Author"(failed) and new Intent for SettingsActivity

--- app/src/main/java/com/example/medicinal_plants/MainActivity.java
+++ app/src/main/java/com/example/medicinal_plants/MainActivity.java
@@ -95,6 +95,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         if(id==R.id.action_settings)
         {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.action_author)
+        {
+            setContentView(R.layout.about_author);
             return true;
         }
 
