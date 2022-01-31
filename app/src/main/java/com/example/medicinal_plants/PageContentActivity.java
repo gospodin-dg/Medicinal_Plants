85ffe610475574953b9433b31fc25b6ad5e0983c
Gospodin-DG <gospodin-dg@yandex.ru>
2022-01-31 23:52:45 +0300

    Local finish work with settings PreferenceScreen

--- app/src/main/java/com/example/medicinal_plants/PageContentActivity.java
+++ app/src/main/java/com/example/medicinal_plants/PageContentActivity.java
@@ -1,11 +1,14 @@
 package com.example.medicinal_plants;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,6 +17,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class PageContentActivity extends AppCompatActivity {
 
+    private ImageView pageContentHeader;
+    private LinearLayout pageContentFooter;
     private ActionBar actionBar;
     private int menu_item = 0;
     private int menu_item_item = 0;
@@ -59,10 +64,10 @@ public class PageContentActivity extends AppCompatActivity {
             actionBar = getSupportActionBar();
         }
         pangolin = Typeface.createFromAsset(getAssets(),"fonts/Pangolin-Regular.ttf");
-        oswald = Typeface.createFromAsset(getAssets(),"fonts/Oswald-VariableFont_wght.ttf");
+        oswald = Typeface.createFromAsset(getAssets(),"fonts/Oswald-Regular.ttf");
         pacifico = Typeface.createFromAsset(getAssets(),"fonts/Pacifico-Regular.ttf");
-        robotomono = Typeface.createFromAsset(getAssets(),"fonts/RobotoMono-VariableFont_wght.ttf");
-//        content_text.setTypeface(pangolin);
+        robotomono = Typeface.createFromAsset(getAssets(),"fonts/RobotoMono-Regular.ttf");
+        content_text.setTypeface(pangolin);
         medicinal_plants_titles = getResources().getStringArray(R.array.medicinal_plants);
         ills_titles = getResources().getStringArray(R.array.ills);
         identifier_titles = getResources().getStringArray(R.array.identifier);
@@ -70,8 +75,15 @@ public class PageContentActivity extends AppCompatActivity {
         collection_storage_titles = getResources().getStringArray(R.array.collection_storage);
         advices_titles = getResources().getStringArray(R.array.advices);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        pageContentHeader = findViewById(R.id.main_content_page_header);
+        pageContentFooter = findViewById(R.id.main_content_page_footer);
+
         setTextSize();
         setTextFont();
+        setTextDecoration();
+        setTextColors();
+        setBackgroundColors();
+        setActionbarColors();
     }
 
     private void setContentText(){
@@ -116,31 +128,31 @@ public class PageContentActivity extends AppCompatActivity {
         }
     }
 
-    private void setTextSize(){
-        String text_size = sharedPreferences.getString("text_size_settings", "15sp");
+    private void setTextSize() {
+        String text_size = sharedPreferences.getString("text_size_settings", "15");
         if(text_size != null){
             switch (text_size){
-                case "10sp":
+                case "10":
                     content_text.setTextSize(10);
                     break;
-                case "15sp":
+                case "15":
                     content_text.setTextSize(15);
                     break;
-                case "20sp":
+                case "20":
                     content_text.setTextSize(20);
                     break;
-                case "25sp":
+                case "25":
                     content_text.setTextSize(25);
                     break;
-                case "30sp":
+                case "30":
                     content_text.setTextSize(30);
                     break;
             }
         }
     }
 
-    private void setTextFont(){
-        String text_font = sharedPreferences.getString("text_font_settings", "Pangolin");
+    private void setTextFont() {
+        String text_font = sharedPreferences.getString("text_font_settings", "Pacifico");
         if(text_font != null){
             switch (text_font){
                 case "Oswald":
@@ -159,5 +171,146 @@ public class PageContentActivity extends AppCompatActivity {
         }
     }
 
+    private void setTextDecoration() {
+        String text_decoration = sharedPreferences.getString("text_style_settings", "Обычный");
+        if(text_decoration != null){
+            switch (text_decoration){
+                case "Жирный":
+                    content_text.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+                    break;
+                case "Курсив":
+                    content_text.setTypeface(Typeface.defaultFromStyle(Typeface.ITALIC));
+                    break;
+                case "Жирный-Курсив":
+                    content_text.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD_ITALIC));
+                    break;
+                case "Обычный":
+                    content_text.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+                    break;
+            }
+        }
+    }
 
+    private void setTextColors() {
+        String text_color = sharedPreferences.getString("text_color_settings", "Черный");
+        if(text_color != null){
+            switch (text_color){
+                case "Черный":
+                    content_text.setTextColor(Color.BLACK);
+                    break;
+                case "Белый":
+                    content_text.setTextColor(Color.WHITE);
+                    break;
+                case "Желтый":
+                    content_text.setTextColor(Color.YELLOW);
+                    break;
+                case "Красный":
+                    content_text.setTextColor(Color.RED);
+                    break;
+                case "Зеленый":
+                    content_text.setTextColor(Color.GREEN);
+                    break;
+                case "Синий":
+                    content_text.setTextColor(Color.BLUE);
+                    break;
+                case "Серый":
+                    content_text.setTextColor(Color.GRAY);
+                    break;
+                case "Стандартный":
+                    content_text.setTextColor(getResources().getColor(R.color.purple_500));
+                    break;
+                case "Фиолетовый":
+                    content_text.setTextColor(getResources().getColor(R.color.purple_200));
+                    break;
+            }
+        }
+    }
+
+    private void setBackgroundColors() {
+        String background_color = sharedPreferences.getString("background_color_settings", "Белый");
+        if(background_color != null){
+            switch (background_color){
+                case "Черный":
+                    content_text.setBackgroundColor(Color.BLACK);
+                    break;
+                case "Белый":
+                    content_text.setBackgroundColor(Color.WHITE);
+                    break;
+                case "Желтый":
+                    content_text.setBackgroundColor(Color.YELLOW);
+                    break;
+                case "Красный":
+                    content_text.setBackgroundColor(Color.RED);
+                    break;
+                case "Зеленый":
+                    content_text.setBackgroundColor(Color.GREEN);
+                    break;
+                case "Синий":
+                    content_text.setBackgroundColor(Color.BLUE);
+                    break;
+                case "Серый":
+                    content_text.setBackgroundColor(Color.GRAY);
+                    break;
+                case "Стандартный":
+                    content_text.setBackgroundColor(getResources().getColor(R.color.purple_500));
+                    break;
+                case "Фиолетовый":
+                    content_text.setBackgroundColor(getResources().getColor(R.color.purple_200));
+                    break;
+            }
+        }
+    }
+
+    private void setActionbarColors() {
+        String actionbar_color = sharedPreferences.getString("actionbar_color_settings", "Стандартный");
+        if(actionbar_color != null){
+            switch (actionbar_color){
+                case "Черный":
+                    actionBar.setBackgroundDrawable(new ColorDrawable(Color.BLACK));
+                    pageContentHeader.setBackgroundColor(Color.BLACK);
+                    pageContentFooter.setBackgroundColor(Color.BLACK);
+                    break;
+                case "Белый":
+                    actionBar.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                    pageContentHeader.setBackgroundColor(Color.WHITE);
+                    pageContentFooter.setBackgroundColor(Color.WHITE);
+                    break;
+                case "Желтый":
+                    actionBar.setBackgroundDrawable(new ColorDrawable(Color.YELLOW));
+                    pageContentHeader.setBackgroundColor(Color.YELLOW);
+                    pageContentFooter.setBackgroundColor(Color.YELLOW);
+                    break;
+                case "Красный":
+                    actionBar.setBackgroundDrawable(new ColorDrawable(Color.RED));
+                    pageContentHeader.setBackgroundColor(Color.RED);
+                    pageContentFooter.setBackgroundColor(Color.RED);
+                    break;
+                case "Зеленый":
+                    actionBar.setBackgroundDrawable(new ColorDrawable(Color.GREEN));
+                    pageContentHeader.setBackgroundColor(Color.GREEN);
+                    pageContentFooter.setBackgroundColor(Color.GREEN);
+                    break;
+                case "Синий":
+                    actionBar.setBackgroundDrawable(new ColorDrawable(Color.BLUE));
+                    pageContentHeader.setBackgroundColor(Color.BLUE);
+                    pageContentFooter.setBackgroundColor(Color.BLUE);
+                    break;
+                case "Серый":
+                    actionBar.setBackgroundDrawable(new ColorDrawable(Color.GRAY));
+                    pageContentHeader.setBackgroundColor(Color.GRAY);
+                    pageContentFooter.setBackgroundColor(Color.GRAY);
+                    break;
+                case "Стандартный":
+                    actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.purple_500)));
+                    pageContentHeader.setBackgroundColor(getResources().getColor(R.color.purple_500));
+                    pageContentFooter.setBackgroundColor(getResources().getColor(R.color.purple_500));
+                    break;
+                case "Фиолетовый":
+                    actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.purple_200)));
+                    pageContentHeader.setBackgroundColor(getResources().getColor(R.color.purple_200));
+                    pageContentFooter.setBackgroundColor(getResources().getColor(R.color.purple_200));
+                    break;
+            }
+        }
+    }
 }
