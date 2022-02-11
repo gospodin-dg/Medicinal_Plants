1b6de6def28b3f2808eb5934f6349008f6175ae4
Gospodin-DG <gospodin-dg@yandex.ru>
2022-02-11 16:50:02 +0300

    Add progressBar on LogoActivity

--- app/src/main/java/com/example/medicinal_plants/LogoActivity.java
+++ app/src/main/java/com/example/medicinal_plants/LogoActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class LogoActivity extends AppCompatActivity {
@@ -19,18 +22,25 @@ public class LogoActivity extends AppCompatActivity {
     private Button buttonStart;
     private TextView titleLogo;
 
+    private ProgressBar progressBar;
+    private TextView progressStatus;
+    private  int progress = 0;
+    private Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logo);
         init();
-//        startMainActivity();
     }
 
     private void init() {
         imageLogo = findViewById(R.id.image_logo);
         buttonStart = findViewById(R.id.button_start);
         titleLogo = findViewById(R.id.title_logo);
+
+        progressBar = findViewById(R.id.progressBar);
+        progressStatus = findViewById(R.id.progressStatus);
 
         logoAnimation = AnimationUtils.loadAnimation(getBaseContext(), R.anim.logo_animation);
         buttonAnimation = AnimationUtils.loadAnimation(getBaseContext(), R.anim.button_animation);
@@ -39,29 +49,54 @@ public class LogoActivity extends AppCompatActivity {
         imageLogo.startAnimation(logoAnimation);
         buttonStart.startAnimation(buttonAnimation);
         titleLogo.startAnimation(titleAnimation);
+
     }
 
-/*    private void startMainActivity() {
+    private void startMainActivity() {
+
         new Thread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    Thread.sleep(5000);
-                    Intent intent = new Intent(LogoActivity.this, MainActivity.class);
-                    startActivity(intent);
+
+               handler.post(new Runnable(){
+                   @Override
+                   public void run() {
+                       buttonStart.setVisibility(View.GONE);
+                       progressBar.setVisibility(View.VISIBLE);
+                       progressStatus.setVisibility(View.VISIBLE);
+                   }
+               });
+
+                for (int i = 0; i < 100; i++) {
+                    progress = i + 1;
+                    SystemClock.sleep(20);
+
+                    handler.post(new Runnable(){
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(progress);
+                            progressStatus.setText(progress + "%");
+                            if(progress == 100){
+                                Intent intent = new Intent(LogoActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        }
+                    });
                 }
-                catch(Exception e) {
-                    e.printStackTrace();
-                }
+
             }
         }).start();
-    }*/
+    }
 
 
     public void onClickStartApp(View view) {
-        Intent intent = new Intent(LogoActivity.this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
+        startMainActivity();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
@@ -69,7 +104,5 @@ public class LogoActivity extends AppCompatActivity {
         super.onStop();
         finish();
     }
-
-
 
 }
